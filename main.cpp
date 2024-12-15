9e06879ad35da6af916c71c2e821e7ad5ba9234f
Sara-Eslam <saraeslam.elgamaal2004@gmail.com>
2024-12-15 17:29:38 +0200

    hello main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 using namespace std;
 int main() {
-	cout << "main";
-  cout<<"dev";
-}
-int add(int a, int b) {
-	return a + b;
-}
-
-int diff(int a, int b) {
-	return a - b;
+	cout << "hello main";
 }
 
